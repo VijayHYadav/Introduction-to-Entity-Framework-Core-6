0eae43bef012fcc73240728059e5c6f557fb891b
VijayHYadav <vizzyadav1997@gmail.com>
2023-09-23 19:29:44 +0530

    10. Configuring EF Core in ASP.NET Core

---
+++ EFCoreASPNETCOREDemo/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EFCoreASPNETCOREDemo.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreASPNETCOREDemo
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Person> People { get; set; }
+    }
+}

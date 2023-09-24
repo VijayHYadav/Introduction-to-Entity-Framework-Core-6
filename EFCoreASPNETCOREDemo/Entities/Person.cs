7fe29e23316220a9d2a1af763c04627d2fa73176
VijayHYadav <vizzyadav1997@gmail.com>
2023-09-24 07:47:42 +0530

    branch setup

--- EFCoreASPNETCOREDemo/Entities/Person.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EFCoreASPNETCOREDemo.Entities
-{
-        public class Person
-        {
-            public int Id { get; set; }
-            public string Name { get; set; }
-            public DateTime DateOfBirth { get; set; }
-        }
-}

c2da2e37c9704162a78d9c97588a68c2e1e93480
VijayHYadav <vizzyadav1997@gmail.com>
2023-09-27 07:13:54 +0530

    36. Fetching a List of Records

---
+++ EFCoreMovies/Controllers/GenresController.cs
@@ -0,0 +1,29 @@
+﻿using Azure;
+using EFCoreMovies.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreMovies.Controllers
+{
+    [ApiController]
+    [Route("api/genres")]
+    public class GenresController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public GenresController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Genre>> Get() {
+            // here we are doing a synchronous operation
+            // But since consulting a database is an io
+            // operation, the good practices says that we should use asynchronous programming
+            // The good thing is that entity framework core can execute asynchronous operations very easily.
+            //return context.Generes.ToList();
+            return await context.Generes.ToListAsync();
+        }
+    }
+}
